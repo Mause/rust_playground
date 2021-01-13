8b28bda34cdee4a1b568215dfa7fee6557f0a270
Elliana <me@mause.me>
2021-01-13 17:35:08 +0700

    Use async fork of google-maps

--- src/google_maps.rs
+++ src/google_maps.rs
@@ -1,25 +1,18 @@
-use futures::executor::block_on;
-use futures::future::ok;
 use google_maps::prelude::ClientSettings;
 use google_maps::Region;
+use tokio_compat_02::FutureExt;
 use std::env;
 
 pub async fn sync_resolve_location(location: &str) -> String {
-    let copied_location = location.to_string();
-
-    let fut = futures::future::lazy(move |_| {
-        let mut client =
-            ClientSettings::new(&env::var("GOOGLE_MAPS_API_KEY").expect("GOOGLE_MAPS_API_KEY"));
-        let res = client
-            .geocoding()
-            .with_address(&copied_location)
-            .with_region(Region::Australia)
-            .execute()
-            .expect("Geocode call failed");
-        ok::<String, String>(res.results[0].formatted_address.to_string())
-    });
-
-    let spawn = futures::executor::spawn_with_handle(fut);
-    let handle = block_on(spawn).unwrap();
-    block_on(handle).unwrap()
+    let client =
+        ClientSettings::new(&env::var("GOOGLE_MAPS_API_KEY").expect("GOOGLE_MAPS_API_KEY"));
+    let res = client
+        .geocoding()
+        .with_address(&location)
+        .with_region(Region::Australia)
+        .execute()
+        .compat()
+        .await
+        .expect("Geocode call failed");
+    res.results[0].formatted_address.to_string()
 }
