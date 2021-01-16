d822fc77cb4c4be517508aba63fb88c90a881d56
Elliana <me@mause.me>
2021-01-16 13:03:55 +0700

    Define SimpleError

--- src/google_maps.rs
+++ src/google_maps.rs
@@ -1,8 +1,23 @@
 use google_maps::prelude::ClientSettings;
 use google_maps::Region;
 use std::env;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 use tokio_compat_02::FutureExt;
 
+#[derive(Debug, Clone)]
+pub struct SimpleError(pub String);
+unsafe impl Send for SimpleError {}
+impl Display for SimpleError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        self.0.fmt(f)
+    }
+}
+impl Error for SimpleError {
+    fn source(&self) -> std::option::Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
 pub async fn sync_resolve_location(location: &str) -> String {
     let client =
         ClientSettings::new(&env::var("GOOGLE_MAPS_API_KEY").expect("GOOGLE_MAPS_API_KEY"));
